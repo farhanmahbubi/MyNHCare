4b405c258ad3f3975973f8a6f26c5f225103037a
mrayhantazakka <rayhantazakka1123@gmail.com>
2023-10-16 14:17:37 +0700

    ngene sek

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mynhcare"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mynhcare"
-        minSdk = 32
-        targetSdk = 33
+        minSdk = 24
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
